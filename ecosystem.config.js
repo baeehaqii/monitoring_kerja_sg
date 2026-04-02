cba32503d714eb22caeb8229c993e717f39ffc49
Unname <muhamadimambaehaqi@gmail.com>
2026-04-02 15:06:47 +0700

    update 2 april tambah proyek dan user sso 4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'task-sg',
+      name: 'tasksg',
       script: 'node_modules/.bin/next',
       args: 'start -p 3003',
       cwd: '/www/wwwroot/task-sg.findy.my.id',
       instances: 1,
       autorestart: true,
       watch: false,
+      max_memory_restart: '400M',
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 3000,
+      error_file: '/www/wwwroot/task-sg.findy.my.id/logs/pm2-error.log',
+      out_file: '/www/wwwroot/task-sg.findy.my.id/logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         NODE_ENV: 'production',
         PORT: 3003,
